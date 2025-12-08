bade9e2bc6685b04dc709b8a945de8267e3caf45
itzan jimenez <holaitzan@gmail.com>
2025-12-08 22:58:27 +0100

    idioma

--- app/src/main/java/com/example/fhubo/CityLocationsActivity.kt
+++ app/src/main/java/com/example/fhubo/CityLocationsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fhubo
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,9 @@ class CityLocationsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btn_back_city = findViewById<ImageButton>(R.id.btn_back_city)
+    btn_back_city.setOnClickListener {
+        finish()
+    }
     }
 }
--- app/src/main/java/com/example/fhubo/User.kt
+++ app/src/main/java/com/example/fhubo/User.kt
@@ -1,10 +1,12 @@
 package com.example.fhubo
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class User : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,11 @@ class User : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar_settings)
+        // Acción para el botón de retroceso en la barra superior
+        toolbar.setOnClickListener {
+            finish() // Cierra la actividad actual y vuelve a la anterior.
         }
     }
 }
