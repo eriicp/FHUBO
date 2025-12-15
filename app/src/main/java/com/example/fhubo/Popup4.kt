25f5fe1e0fd75a4a9ba9ac71e063b5f54769c3ac
itzan jimenez <holaitzan@gmail.com>
2025-12-15 18:40:12 +0100

    idioma

--- app/src/main/java/com/example/fhubo/Popup4.kt
+++ app/src/main/java/com/example/fhubo/Popup4.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.button.MaterialButton
 
 class Popup4 : AppCompatActivity() {
