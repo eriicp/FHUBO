892e883828024ff2f43225b34f04c2ffd3b5b9e6
itzan jimenez <holaitzan@gmail.com>
2025-12-15 18:39:47 +0100

    idioma

---
+++ app/src/main/java/com/example/fhubo/Popup4.kt
@@ -0,0 +1,39 @@
+package com.example.fhubo
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+
+class Popup4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_popup4)
+        val btnBack = findViewById<MaterialButton>(R.id.btn_back)
+        val btnNext = findViewById<MaterialButton>(R.id.btn_next)
+        val btn_skip = findViewById<MaterialButton>(R.id.btn_skip)
+
+        btn_skip.setOnClickListener {
+            // Vuelve al popup anterior (Popup3)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        btnBack.setOnClickListener {
+            // Vuelve al popup anterior (Popup3)
+            val intent = Intent(this, Popup3::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnNext.setOnClickListener {
+            // Avanza al último popup (Popup5)
+            val intent = Intent(this, Popup5::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
